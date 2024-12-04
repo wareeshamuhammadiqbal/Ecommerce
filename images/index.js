d7c2c2d043b6e14adebbde28b7e2c1e0ba60a97a
wareeshamuhammadiqbal <alighanchi2323@gmail.com>
2024-12-03 17:41:14 -0800

    Ecommerce

---
+++ images/index.js
@@ -0,0 +1,18 @@
+let card = document.querySelectorAll(".trend");
+
+let blog = document.querySelector(".trends");
+
+let mainpage = document.querySelector(".main");
+
+function shops(){
+
+mainpage.style.display = "none";
+
+blog.style.display = "none";
+    // card.style.display =  
+}
+
+function blog(){
+    mainpage.style.display = "none";
+    card.style.display = "none";
+}
